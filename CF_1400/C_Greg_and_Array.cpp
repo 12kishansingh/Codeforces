a82772796829089fc4cc8b0e3b6f2bafb27fe069
12kishansingh <ks888singh@gmail.com>
2024-07-28 15:39:54 +0530

    2

--- CF_1400/C_Greg_and_Array.cpp
+++ CF_1400/C_Greg_and_Array.cpp
@@ -20,9 +20,46 @@ return ostream;
 
 
 void solve(){
-int n;cin>>n;
-vector<int>a(n);
-cin>>a;
+ int n, m, k;
+    cin >> n >> m >> k;
+
+    vector<long long> a(n + 1);
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+
+    vector<int> l(m + 1), r(m + 1);
+    vector<long long> d(m + 1);
+    for (int i = 1; i <= m; i++) {
+        cin >> l[i] >> r[i] >> d[i];
+    }
+
+    vector<long long> opCount(m + 2, 0);
+    for (int i = 0; i < k; i++) {
+        int x, y;
+        cin >> x >> y;
+        opCount[x]++;
+        opCount[y + 1]--;
+    }
+
+    for (int i = 1; i <= m; i++) {
+        opCount[i] += opCount[i - 1];
+    }
+    vector<long long> diff(n + 2, 0);
+    for (int i = 1; i <= m; i++) {
+        if (opCount[i] > 0) {
+            diff[l[i]] += d[i] * opCount[i];
+            diff[r[i] + 1] -= d[i] * opCount[i];
+        }
+    }
+    for (int i = 1; i <= n; i++) {
+        diff[i] += diff[i - 1];
+        a[i] += diff[i];
+    }
+
+    for (int i = 1; i <= n; i++) {
+        cout << a[i] << " ";
+    }
 
 }
 int main(){
@@ -30,8 +67,8 @@ ios_base::sync_with_stdio(false);
 cin.tie(0);
 cout.tie(0);
 int t;
-cin>>t;
-
+//cin>>t;
+t=1;
 while(t--){
 solve();
 }
